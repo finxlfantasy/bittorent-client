8f4cd0c988166b76ef7d499c1d66c91b10c1e74e
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-09-25 17:37:52 +0200

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use serde_json;
 use std::env;
 
 // Available if you need it!
-// use serde_bencode
+struct Decoded {
+    value: serde_json::Value,
+    length: usize,
+}
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> Decoded {
     // If encoded_value starts with a digit, it's a number
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
@@ -13,13 +16,29 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+        Decoded {
+            length: string.len() + colon_index + 1,
+            value: serde_json::Value::String(string.to_string()),
+        }
     } else if encoded_value.chars().next().unwrap() == 'i' {
-        // Example: "5:hello" -> "hello"
+         //i.45 =  45
         let e_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..e_index];
         let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(serde_json::Number::from(number));
+        Decoded {
+            length: number_string.len() +2,
+            value: serde_json::Value::Number(serde_json::Number::from(number)),
+        }
+    } else if encoded_value.starts_with('l') {
+        // l5:helloi52ee  -> l5:helloi52ee ["hello", 52] 
+        let mut list = Vec::new();
+        let mut index =1;
+        while encoded_value.chars().nth(index).unwrap() != 'e' {
+            let decoded_value = decode_bencoded_value(&encoded_value[index..]);
+            list.push(decoded_value.value);
+            index += decoded_value.length;
+        }
+    Decoded { value: serde_json::Value::Array(list), length: index + 1, }
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -35,7 +54,7 @@ fn main() {
         // println!("Logs from your program will appear here!");
 
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = decode_bencoded_value(encoded_value).value;
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1]) 
