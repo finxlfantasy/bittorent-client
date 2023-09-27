d1bd9219d9e20fbed8062a06470c035f81b44c5b
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-09-27 13:33:45 +0200

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,56 @@
-use serde_json;
+use serde_bencode::{de, value::Value as BencodeValue};
+use serde_bytes::ByteBuf;
+use serde_json::{Map, Value as JsonValue};
+use serde::Deserialize;
 use std::env;
 
-// Available if you need it!
-struct Decoded {
-    value: serde_json::Value,
-    length: usize,
+#[derive(Debug, Deserialize)]
+struct Torrent {
+    announce: String,
+    info: TorrentInfo,
 }
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> Decoded {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        Decoded {
-            length: string.len() + colon_index + 1,
-            value: serde_json::Value::String(string.to_string()),
-        }
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-         //i.45 =  45
-        let e_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..e_index];
-        let number = number_string.parse::<i64>().unwrap();
-        Decoded {
-            length: number_string.len() +2,
-            value: serde_json::Value::Number(serde_json::Number::from(number)),
-        }
-    } else if encoded_value.starts_with('l') {
-        // l5:helloi52ee  -> l5:helloi52ee ["hello", 52] 
-        let mut list = Vec::new();
-        let mut index = 1;
-        while encoded_value.chars().nth(index).unwrap() != 'e' {
-            let decoded_value = decode_bencoded_value(&encoded_value[index..]);
-            list.push(decoded_value.value);
-            index += decoded_value.length;
-        }
+#[derive(Debug, Deserialize)]
+struct TorrentInfo {
+    length: usize,
+    name: String,
+    #[serde(rename = "piece length")]
+    pieces_length: usize,
+    pieces: ByteBuf,
+}
 
-    Decoded { 
-        value: serde_json::Value::Array(list), 
-        length: index + 1, 
-    } 
-    } else if encoded_value.starts_with('d') {
-        // d3:foo3:bar5:helloi52ee  -> {"hello": 52, "foo":"bar"} 
-        let mut map = serde_json::Map::new();
-        let mut index = 1;
-        while encoded_value.chars().nth(index).unwrap() != 'e' {
-            let encoded_value = &encoded_value[index..];
-            let decoded_key = decode_bencoded_value(encoded_value);
-            let decoded_value = decode_bencoded_value(&encoded_value[decoded_key.length..]);
-            map.insert(
-                decoded_key.value.as_str().unwrap().to_string(),
-                decoded_value.value,
-            );
-            index += decoded_value.length + decoded_key.length;
+fn to_json(value: &BencodeValue) -> JsonValue {
+    match value {
+        BencodeValue::Bytes(bytes) => JsonValue::String(String::from_utf8_lossy(bytes).to_string()),
+        BencodeValue::Int(num) => JsonValue::Number(num.to_owned().into()),
+        BencodeValue::List(list) => JsonValue::Array(list.iter().map(|v| to_json(v)).collect()),
+        BencodeValue::Dict(dict) => {
+            let mut json_dict = Map::new();
+            for (key, val) in dict.iter(){
+                let key = String::from_utf8(key.clone().into()).unwrap().to_string();
+                let val = to_json(val);
+                json_dict.insert(key, val);
+            }
+            JsonValue::Object(json_dict)
         }
-        Decoded {
-            value: serde_json::Value::Object(map),
-            length : index + 1,
-        }
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
-
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // println!("Logs from your program will appear here!");
-
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value).value;
-        println!("{}", decoded_value.to_string());
+        let decoded_value: BencodeValue = de::from_str(encoded_value).unwrap();
+        println!("{}", to_json(&decoded_value)); 
+    } else if command  == "TorrentInfo" {
+        let torrent_file = &args[2];
+        let file_buf = std::fs::read(torrent_file).unwrap();
+        let torrent = de::from_bytes::<Torrent>(&file_buf).unwrap(); 
+
+        println!("Tracker URL: {}", torrent.announce);
+        println!("Length: {}", torrent.info.length);
     } else {
         println!("unknown command: {}", args[1]) 
     }
