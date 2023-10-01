49be3ce3959a53748bcebc0935c455d75c8ad410
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-10-01 16:39:08 +0200

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde::{Serialize, Deserialize};
 use std::env;
 use sha1::{Digest, Sha1};
 use hex;
+use url::form_urlencoded;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Torrent {
@@ -37,17 +38,45 @@ fn to_json(value: &BencodeValue) -> JsonValue {
     }
 }
 
-
 fn info_hash(info: &Info) -> String {
     let mut hasher = Sha1::new();
     hasher.update(serde_bencode::to_bytes(&info).unwrap());
     hex::encode(hasher.finalize())
 }
 
+fn make_tracker_request(tracker_url: &str, info_hash: &str, peer_id: &str, total_length: usize) -> Result<(), reqwest::Error> {
+    let left = total_length.to_string();
+
+    let mut params = form_urlencoded::Serializer::new(String::new());
+    params.append_pair("info_hash", info_hash);
+    params.append_pair("peer_id", peer_id);
+    params.append_pair("port", "6881");
+    params.append_pair("uploaded", "0");
+    params.append_pair("downloaded", "0");
+    params.append_pair("left", &left);
+    params.append_pair("compact", "1");
+
+    let request_url =format!("{}?{}", tracker_url, params.finish());
+
+    //Mak the GET request to the tracker
+    let response= reqwest::blocking::get(&request_url)?;
+
+    if response.status().is_success() {
+        let response_body = response.bytes()?;
+        println!("Response body: {:?}", response_body);
+    } else {
+        println!("Error: Request failed with status {:?}", response.status());
+    }
+    Ok(())
+
+}
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
+
+    let peer_id = "00112233445566778899";
+    let total_length = 12335;
 
     if command == "decode" {
         let encoded_value = &args[2];
@@ -57,16 +86,23 @@ fn main() {
         let file_name = &args[2];
         let file_buf = std::fs::read(file_name).unwrap();
         let torrent = de::from_bytes::<Torrent>(&file_buf).unwrap(); 
-    
+
+        let tracker_url = &torrent.announce;
+        let info_hash = info_hash(&torrent.info);
+
         println!("Tracker URL: {}", torrent.announce);
         println!("Length: {}", torrent.info.length);
-        println!("Info Hash: {}", info_hash(&torrent.info));
+        println!("Info Hash: {}", info_hash);
         println!("Piece Length: {}", torrent.info.pieces_length);
 
-        println!("Pieces Hash: ");
+        println!("Piece Hash: ");
         for chunk in torrent.info.pieces.chunks(20) {
             let hash_hex: String = hex::encode(chunk);
             println!("{}", hash_hex);
+        }
+
+        if let Err(err) = make_tracker_request(tracker_url, &info_hash, peer_id, total_length) {
+            println!("Error: {:?}", err);
         }
     } else {
         println!("unknown command: {}", args[1]) 
